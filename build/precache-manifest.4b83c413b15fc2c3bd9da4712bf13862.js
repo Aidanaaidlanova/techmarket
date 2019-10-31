221c1d951759f95a2a3488d5e2a6dd7b93174a4a
Chpokman <cusenov785@gmail.com>
2019-10-31 08:24:49 +0600

    i made "calculator" in a cart (part 2)

---
+++ build/precache-manifest.4b83c413b15fc2c3bd9da4712bf13862.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d7b2ff5d6b4f1288fbddf3610606494",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc51a5fb0a8a7049ee50",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "1057ee5f7a1cb7859975",
+    "url": "/static/css/main.d92db735.chunk.css"
+  },
+  {
+    "revision": "dc51a5fb0a8a7049ee50",
+    "url": "/static/js/2.123f0672.chunk.js"
+  },
+  {
+    "revision": "1057ee5f7a1cb7859975",
+    "url": "/static/js/main.ae4e292c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "af7b6e740207d49f58b9482bb2d25587",
+    "url": "/static/media/banner_background.af7b6e74.jpg"
+  },
+  {
+    "revision": "a9bb6fa9ceafec3d528fb9adb265fa80",
+    "url": "/static/media/cart.a9bb6fa9.png"
+  },
+  {
+    "revision": "1aab95d1ed2eb58ba8b01275935634a0",
+    "url": "/static/media/fa-brands-400.1aab95d1.svg"
+  },
+  {
+    "revision": "3189f3e1d3b91f842e3aa647c7728bfe",
+    "url": "/static/media/fa-brands-400.3189f3e1.woff2"
+  },
+  {
+    "revision": "951e024f1f7cfe33c4838a3c146d5408",
+    "url": "/static/media/fa-brands-400.951e024f.ttf"
+  },
+  {
+    "revision": "bf6522fea650fffa9a4ca3f2fc6c5062",
+    "url": "/static/media/fa-brands-400.bf6522fe.eot"
+  },
+  {
+    "revision": "df6b6c3900c43cafe91cdaa5caa39c71",
+    "url": "/static/media/fa-brands-400.df6b6c39.woff"
+  },
+  {
+    "revision": "17b7624171b4b447d2e783a88a016391",
+    "url": "/static/media/fa-regular-400.17b76241.woff"
+  },
+  {
+    "revision": "1fe5d4b1620f9144c434b60a702c7592",
+    "url": "/static/media/fa-regular-400.1fe5d4b1.svg"
+  },
+  {
+    "revision": "57036b128ae0c649e364c581ce450970",
+    "url": "/static/media/fa-regular-400.57036b12.woff2"
+  },
+  {
+    "revision": "72ca8dd93f526b4f21e3e39aa79d9bcc",
+    "url": "/static/media/fa-regular-400.72ca8dd9.ttf"
+  },
+  {
+    "revision": "77f203215b127bbbf1028a0b90d1c73a",
+    "url": "/static/media/fa-regular-400.77f20321.eot"
+  },
+  {
+    "revision": "4194be4b16699db0db223f40060f661f",
+    "url": "/static/media/fa-solid-900.4194be4b.eot"
+  },
+  {
+    "revision": "60e625f3538c9e9aa2a955a6994156fc",
+    "url": "/static/media/fa-solid-900.60e625f3.woff"
+  },
+  {
+    "revision": "6ae956a36625015a447446e8f489f267",
+    "url": "/static/media/fa-solid-900.6ae956a3.woff2"
+  },
+  {
+    "revision": "6b31859a7574498788fce508b391ec80",
+    "url": "/static/media/fa-solid-900.6b31859a.svg"
+  },
+  {
+    "revision": "eb1e2d8f80107f26e5ba6fdce5ea4fe3",
+    "url": "/static/media/fa-solid-900.eb1e2d8f.ttf"
+  },
+  {
+    "revision": "831017c3ca0e7050561edd5c3f0bf620",
+    "url": "/static/media/shop_background.831017c3.jpg"
+  }
+]);
